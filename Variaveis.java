db72ede9237bf1d0f4af5b7b26717b36d3828528
Yasminpiornedo <159542694+Yasminpiornedo@users.noreply.github.com>
2025-08-07 09:54:03 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,20 @@
+public class Variaveis {
+    
+    public static void main(String[]args){
+
+       //tipo nome = conteudo;
+
+       String meuNome = "Edson ";
+       meuNome += "Fernandes";
+       int idade = 50;
+       double altura = 1.79;
+       char sexo = 'M';
+       boolean casado = true; 
+
+
+       System.out.prin("Meu nome é "+meuNome);
+       System.out.print(" minha idade  é "+idade);
+       System.out.print(" anos e minha altura é "+altura);
+       
+    }
+}
